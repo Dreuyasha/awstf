2e25b29c9bdc7ca5c79f78cf87783a933dec26c7
Dreuyasha <110018054+Dreuyasha@users.noreply.github.com>
2025-05-12 18:38:47 -0400

    hello

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.dreVpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
+  availability_zone       = element(var.availability_zones, count.index % 2)
   map_public_ip_on_launch = true
 }
 
@@ -61,4 +61,3 @@ resource "aws_route_table_association" "privateRoutes" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.privateRTB.id
 }
-
